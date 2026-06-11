18fcfcbdd481b55ff6d18478dbbee73cdb049cf8
Ahsanul Haque Chowdhury <ahsan.chowdhury202@gmail.com>
2026-06-11 21:28:38 +0600

    card flipped done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Card } from "./components/Card";
 import { GameHeader } from "./components/GameHeader";
-
-
+import { useState } from "react";
+import { useEffect } from "react";
 const cardValues = [ 
   "🍎",
   "🍌",
@@ -22,14 +22,57 @@ const cardValues = [
 ]
 
 function App() {
+  const [cards, setCards] = useState([]);
+
+  const initializeGame = () => {
   
+
+   
+   const finalCards = cardValues.map((value, index) => (
+
+{
+  id: index,
+  value,
+  isFlipped: false,
+  isMatched: false
+}
+
+    ))
+    setCards(finalCards);
+
+  }
+
+useEffect(() => {
+  initializeGame();
+}, [])
+
+
+const handleCardClick = (card) => {
+  if (card.isFlipped || card.isMatched) {
+    return;
+  }
+const newCards = cards.map((c) => {
+if (c.id === card.id) {
+  return {...c, isFlipped: true};
+}
+else {
+  return c;
+}
+});
+
+setCards(newCards);
+
+}
+
+
+
   return (
   <div className="App"> 
   <GameHeader score={3} moves={10} /> 
 
 <div className="cards-grid">
-{cardValues.map((card) => (
-<Card card={card} />
+{cards.map((card) => (
+<Card card={card} onClick={handleCardClick} />
 
 
 ))}
